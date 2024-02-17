58e925a83626a42313476773a6daa6ec50718681
Tolkinxon <krosstony639@gmail.com>
2024-02-17 17:12:06 +0300

    js side

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -3,23 +3,35 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const burgerBtn = document.querySelector('.header__burger')
     const menuContainer = document.querySelector('.header')
     const choosing__menu = document.querySelectorAll('.choosing__left-item')
+
+    const radioBtn = () => {
+        if (choosing__menu) {
+            choosing__menu.forEach(item => {
+                item.classList.remove('choosing__left-item--active')
+                item.addEventListener('click', (e) => {
+                    radioBtn() 
+                    item.classList.add('choosing__left-item--active')
+                    // if(e.target == )
+                    console.log(item.parentElement);
+                    console.log(e.target.tagName);
+                })
+            })
+        }
+    }
     
     if (burgerBtn) {
         burgerBtn.addEventListener('click', () => {
             menuContainer.classList.toggle('burger--click')
         })
     }
-    
-    
-    if (choosing__menu) {
-        choosing__menu.forEach(item => {
-            item.addEventListener('click', () => {
-                choosing__menu.classList.toggle('choosing__left-item--active')
-            })
-        })
-    }
 
-    console.log(choosing__menu);
+    radioBtn() 
+    
+    
+
+
+
+
   
 });
 
