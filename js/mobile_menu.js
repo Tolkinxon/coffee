dadeb39bdcb9a3f0bff2db82091b717d4adf14b6
Tolkinxon <krosstony639@gmail.com>
2024-02-19 20:42:25 +0300

    some addings

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -3,6 +3,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const burgerBtn = document.querySelector('.header__burger')
     const menuContainer = document.querySelector('.header')
     const choosing__menu = document.querySelectorAll('.choosing__left-item')
+    const choosing__icon = document.querySelectorAll('.choosing__icon')
+    const choosing__icon_mover = document.querySelectorAll('.choosing__icon-mover')
+    
     
     const radioBtn = () => {
         if (choosing__menu) {
@@ -15,7 +18,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             menuContainer.classList.toggle('burger--click')
         })
     }
-
+    
     radioBtn()
     
     if (choosing__menu) {
@@ -25,9 +28,30 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 e.currentTarget.classList.add('choosing__left-item--active')
             })
         })
+    } 
+    
+    if (choosing__icon && choosing__icon_mover) {
+        for(let i = 0; i < choosing__icon.length; i++){
+            choosing__icon[i].addEventListener('click', (e) => {
+                console.log(e);
+                let j = 250
+                
+                const inter = setInterval(() => {
+                    j = j - 5
+                    choosing__icon_mover[i].style.height = `${j}px`
+                    if(j === 0) {
+                        
+                        clearInterval(inter)
+                    }
+                },0.1)
+            })
+        }
     }
     
-     
+    
+    
+    
+    
     
     
     
